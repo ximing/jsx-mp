7448ff35e0d0aaceb8f29838e4b1927980fbd90a
ximing <807754634@qq.com>
2019-03-24 21:32:10 +0800

    feat: 修复运行时问题

--- packages/jsx-runtime/src/index.js
+++ packages/jsx-runtime/src/index.js
@@ -1,129 +1,189 @@
 /**
  * Created by ximing on 2019-03-23.
  */
-import { isEmptyObject, noop, diffObjToPath } from './util'
+import { isEmptyObject, noop, diffObjToPath } from "./util";
 
-const privatePropValName = '__triggerObserer';
-const anonymousFnNamePreffix = 'funPrivate'
-const componentFnReg = /^__fn_/
+const privatePropValName = "__triggerObserer";
+const componentFnReg = /^__fn_/;
 
-function filterProps (properties, defaultProps = {}, componentProps = {}, weappComponentData) {
-    let newProps = Object.assign({}, componentProps)
-    for (const propName in properties) {
-        if (propName === privatePropValName) {
-            continue
-        }
-        if (typeof componentProps[propName] === 'function') {
-            newProps[propName] = componentProps[propName]
-        } else if (propName in weappComponentData) {
-            newProps[propName] = weappComponentData[propName]
-        }
-        if (componentFnReg.test(propName)) {
-            if (weappComponentData[propName] === true) {
-                const fnName = propName.replace(componentFnReg, '')
-                newProps[fnName] = noop
-            }
-            delete newProps[propName]
-        }
+function filterProps(
+  properties,
+  defaultProps = {},
+  componentProps = {},
+  weappComponentData
+) {
+  const newProps = Object.assign({}, componentProps);
+  for (const propName in properties) {
+    if (propName === privatePropValName) {
+      continue;
     }
-    if (!isEmptyObject(defaultProps)) {
-        for (const propName in defaultProps) {
-            if (newProps[propName] === undefined || newProps[propName] === null) {
-                newProps[propName] = defaultProps[propName]
-            }
-        }
+    if (typeof componentProps[propName] === "function") {
+      newProps[propName] = componentProps[propName];
+    } else if (propName in weappComponentData) {
+      newProps[propName] = weappComponentData[propName];
     }
-    return newProps
+    if (componentFnReg.test(propName)) {
+      if (weappComponentData[propName] === true) {
+        const fnName = propName.replace(componentFnReg, "");
+        newProps[fnName] = noop;
+      }
+      delete newProps[propName];
+    }
+  }
+  if (!isEmptyObject(defaultProps)) {
+    for (const propName in defaultProps) {
+      if (newProps[propName] === undefined || newProps[propName] === null) {
+        newProps[propName] = defaultProps[propName];
+      }
+    }
+  }
+  return newProps;
 }
 
-function bindProperties (weappComponentConf, ComponentClass) {
-    weappComponentConf.properties = ComponentClass.properties || {}
-    const defaultProps = ComponentClass.defaultProps || {}
-    for (const key in defaultProps) {
-        if (defaultProps.hasOwnProperty(key)) {
-            weappComponentConf.properties[key] = {
-                type: null,
-                value: null,
-                observer:function () {
-                    if (!this.__isReady) return;
-                    const nextProps = filterProps(ComponentClass.properties, ComponentClass.defaultProps, this.props, this.data);
-                    this.props = nextProps;
-                }
-            }
-        }
+function generateObserver(key, ComponentClass, context) {
+  function observer(value) {
+    const nextProps = filterProps(
+      ComponentClass.properties,
+      ComponentClass.defaultProps,
+      Object.assign({}, this.props, {
+        [key]: value
+      }),
+      this.data
+    );
+    if (this.__isReady) {
+      const oldData = Object.assign({}, this.data);
+      // 这里用context不用this的原因是微信会将传递给Component的Object处理一下，其中不在method里面的方法会被移走
+      const newData = context._createData(oldData, nextProps);
+      const dataDiff = diffObjToPath(newData, this.data);
+      this.__nativeSetData(dataDiff);
     }
-    // // 拦截props的更新，插入生命周期
-    // // 调用小程序setData或会造成性能消耗
-    // weappComponentConf.properties[privatePropValName] = {
-    //     type: null,
-    //     observer: function () {
-    //         if (!this.__isReady) return;
-    //         const nextProps = filterProps(ComponentClass.properties, ComponentClass.defaultProps, this.props, this.data);
-    //         this.props = nextProps;
-    //     }
-    // }
+    this.props = nextProps;
+  }
+  return observer;
 }
 
-function setData(data,fn=noop){
-    let newData = this._createData(data);
+function bindProperties(weappComponentConf, ComponentClass) {
+  weappComponentConf.properties = ComponentClass.properties || {};
+  weappComponentConf.props = weappComponentConf.props || {};
+  Object.keys(weappComponentConf.properties).forEach(key => {
+    weappComponentConf.properties[key] = {
+      ...weappComponentConf.properties[key],
+      value: weappComponentConf.props[key],
+      observer: generateObserver(key, ComponentClass, weappComponentConf)
+    };
+  });
+  const defaultProps = ComponentClass.defaultProps || {};
+  for (const key in defaultProps) {
+    if (defaultProps.hasOwnProperty(key)) {
+      weappComponentConf.properties[key] = {
+        type: null,
+        value: null,
+        observer: generateObserver(key, ComponentClass, weappComponentConf)
+      };
+    }
+  }
+}
+
+function setData(data, fn = noop) {
+  try {
+    const newData = this._createData(Object.assign({}, this.data, data));
     const dataDiff = diffObjToPath(newData, this.data);
-    this._nativeSetData(dataDiff,fn);
+    this.__nativeSetData(dataDiff, fn);
+  } catch (err) {
+    console.error("--> setData error", err);
+    throw err;
+  }
 }
 
-export default function createComponent (ComponentClass) {
-    let initData = {
-        _componentProps: 1
-    }
-    // const componentProps = filterProps({}, ComponentClass.defaultProps)
-    const componentInstance = new ComponentClass()
-    try {
-        componentInstance.data = componentInstance._createData() || componentInstance.data
-    } catch (err) {
-        console.warn(`[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`)
-        console.warn(err)
-    }
-    const weappComponentConf = Object.assign({},componentInstance,{
-        data: Object.assign({}, initData, componentInstance.props, componentInstance.data),
-        __isReady:false,
-        created (...args) {
-            this._nativeSetData = this.setData;
-            this.setData = setData.bind(this);
-            componentInstance.created && componentInstance.created.apply(this,args)
-        },
-        ready(...args){
-            this.__isReady = true;
-            componentInstance.ready && componentInstance.ready.apply(this,args)
-        }
+export function createComponent(ComponentClass) {
+  const initData = {
+    _componentProps: 1
+  };
+  const componentInstance = new ComponentClass();
+  try {
+    componentInstance.data =
+      componentInstance._createData() || componentInstance.data;
+  } catch (err) {
+    console.warn(
+      `[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`
+    );
+    console.warn(err);
+  }
+  const { created, ready, props, data } = componentInstance;
+  componentInstance.data = Object.assign({}, initData, props, data);
+  componentInstance.__isReady = false;
+  componentInstance.created = function(...args) {
+    this.__nativeSetData = this.setData;
+    Object.defineProperty(this, "setData", {
+      value: setData.bind(this),
+      configurable: true,
+      enumerable: true,
+      writable: false
     });
-    bindProperties(weappComponentConf, ComponentClass)
-
-    return weappComponentConf
+    this.props = filterProps(
+      ComponentClass.properties,
+      ComponentClass.defaultProps,
+      this.props,
+      this.data
+    );
+    created && created.apply(this, args);
+  };
+  componentInstance.ready = function(...args) {
+    this.__isReady = true;
+    ready && ready.apply(this, args);
+  };
+  // 将原型链上不可枚举的方法赋值过来
+  Object.getOwnPropertyNames(ComponentClass.prototype).forEach(key => {
+    if (
+      ["_constructor", "data", "props", "created", "ready"].indexOf(key) < 0 &&
+      !Object.prototype.hasOwnProperty(key)
+    )
+      componentInstance[key] = ComponentClass.prototype[key];
+  });
+  bindProperties(componentInstance, ComponentClass);
+  return componentInstance;
 }
 
-
-export default function createPage (ComponentClass) {
-    let initData = {
-        _componentProps: 1
-    }
-    const componentInstance = new ComponentClass()
-    try {
-        componentInstance.data = componentInstance._createData() || componentInstance.data
-    } catch (err) {
-        console.warn(`[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`)
-        console.warn(err)
-    }
-    const weappComponentConf = Object.assign({},componentInstance,{
-        data: Object.assign({}, initData, componentInstance.props, componentInstance.data),
-        __isReady:false,
-        onLoad (...args) {
-            this._nativeSetData = this.setData;
-            this.setData = setData.bind(this);
-            componentInstance.onLoad && componentInstance.onLoad.apply(this,args)
-        },
-        onReady(...args){
-            this.__isReady = true;
-            componentInstance.onReady && componentInstance.onReady.apply(this,args)
-        }
+export function createPage(ComponentClass) {
+  const initData = {
+    _componentProps: 1
+  };
+  const componentInstance = new ComponentClass();
+  try {
+    componentInstance.data =
+      componentInstance._createData() || componentInstance.data;
+  } catch (err) {
+    console.warn(
+      `[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`
+    );
+    console.warn(err);
+  }
+  const { onLoad, onReady, props, data } = componentInstance;
+  componentInstance.data = Object.assign({}, initData, props, data);
+  componentInstance.__isReady = false;
+  componentInstance.onLoad = function(...args) {
+    this.__nativeSetData = this.setData;
+    Object.defineProperty(this, "setData", {
+      value: setData.bind(this),
+      configurable: true,
+      enumerable: true,
+      writable: false
     });
-    return weappComponentConf
+    onLoad && onLoad.apply(this, args);
+  };
+  componentInstance.onReady = function(...args) {
+    this.__isReady = true;
+    onReady && onReady.apply(this, args);
+  };
+  // 将原型链上不可枚举的方法赋值过来
+  Object.getOwnPropertyNames(ComponentClass.prototype).forEach(key => {
+    if (
+      ["_constructor", "data", "props", "onReady", "onReady"].indexOf(key) <
+        0 &&
+      !Object.prototype.hasOwnProperty(key)
+    )
+      componentInstance[key] = ComponentClass.prototype[key];
+  });
+
+  return componentInstance;
 }
--- packages/jsx-runtime/src/util.js
+++ packages/jsx-runtime/src/util.js
@@ -1,15 +1,15 @@
-import isPlainObject from 'lodash/isPlainObject'
+import isPlainObject from "lodash/isPlainObject";
 
-export function isEmptyObject (obj) {
+export function isEmptyObject(obj) {
   if (!obj || !isPlainObject(obj)) {
-    return false
+    return false;
   }
   for (const n in obj) {
     if (obj.hasOwnProperty(n)) {
-      return false
+      return false;
     }
   }
-  return true
+  return true;
 }
 
 /**
@@ -17,238 +17,237 @@ export function isEmptyObject (obj) {
  * @param {Object | Json} jsonObj json对象
  * @return {Object | Json} 新的json对象
  */
-export function objClone (jsonObj) {
-  let buf
+export function objClone(jsonObj) {
+  let buf;
   if (Array.isArray(jsonObj)) {
-    buf = []
-    let i = jsonObj.length
+    buf = [];
+    let i = jsonObj.length;
     while (i--) {
-      buf[i] = objClone(jsonObj[i])
+      buf[i] = objClone(jsonObj[i]);
     }
-    return buf
-  } else if (isPlainObject(jsonObj)) {
-    buf = {}
-    for (const k in jsonObj) {
-      buf[k] = objClone(jsonObj[k])
-    }
-    return buf
-  } else {
-    return jsonObj
+    return buf;
   }
+  if (isPlainObject(jsonObj)) {
+    buf = {};
+    for (const k in jsonObj) {
+      buf[k] = objClone(jsonObj[k]);
+    }
+    return buf;
+  }
+  return jsonObj;
 }
 
-export function getPrototype (obj) {
+export function getPrototype(obj) {
   /* eslint-disable */
   if (Object.getPrototypeOf) {
-    return Object.getPrototypeOf(obj)
+    return Object.getPrototypeOf(obj);
   } else if (obj.__proto__) {
-    return obj.__proto__
+    return obj.__proto__;
   }
   /* eslint-enable */
-  return obj.constructor.prototype
+  return obj.constructor.prototype;
 }
 
-export function getPrototypeChain (obj) {
-  const protoChain = []
+export function getPrototypeChain(obj) {
+  const protoChain = [];
   while ((obj = getPrototype(obj))) {
-    protoChain.push(obj)
+    protoChain.push(obj);
   }
-  return protoChain
+  return protoChain;
 }
 
-export function noop () {}
+export function noop() {}
 
-export function isFunction (arg) {
-  return typeof arg === 'function'
+export function isFunction(arg) {
+  return typeof arg === "function";
 }
 
-export function isArray (arg) {
-  return Array.isArray(arg)
+export function isArray(arg) {
+  return Array.isArray(arg);
 }
 
-export function shakeFnFromObject (obj) {
-  let newObj
+export function shakeFnFromObject(obj) {
+  let newObj;
   if (isArray(obj)) {
-    newObj = []
-    const len = obj.length
+    newObj = [];
+    const len = obj.length;
     for (let i = 0; i < len; i++) {
-      newObj.push(shakeFnFromObject(obj[i]))
+      newObj.push(shakeFnFromObject(obj[i]));
     }
   } else if (isPlainObject(obj)) {
-    newObj = {}
+    newObj = {};
     for (const key in obj) {
       if (isFunction(obj[key])) {
-        continue
+        continue;
       }
-      const ret = shakeFnFromObject(obj[key])
-      newObj[key] = ret
+      const ret = shakeFnFromObject(obj[key]);
+      newObj[key] = ret;
     }
   } else {
-    return obj
+    return obj;
   }
-  return newObj
+  return newObj;
 }
 
-const keyList = Object.keys
-const hasProp = Object.prototype.hasOwnProperty
+const keyList = Object.keys;
+const hasProp = Object.prototype.hasOwnProperty;
 
-function diffArrToPath (to, from, res = {}, keyPrev = '') {
-  const len = to.length
+function diffArrToPath(to, from, res = {}, keyPrev = "") {
+  const len = to.length;
   for (let i = 0; i < len; i++) {
-    const toItem = to[i]
-    const fromItem = from[i]
-    const targetKey = `${keyPrev}[${i}]`
+    const toItem = to[i];
+    const fromItem = from[i];
+    const targetKey = `${keyPrev}[${i}]`;
     if (toItem === fromItem) {
-      continue
+      continue;
     } else if (typeof toItem !== typeof fromItem) {
-      res[targetKey] = toItem
+      res[targetKey] = toItem;
+    } else if (typeof toItem !== "object") {
+      res[targetKey] = toItem;
     } else {
-      if (typeof toItem !== 'object') {
-        res[targetKey] = toItem
-      } else {
-        const arrTo = isArray(toItem)
-        const arrFrom = isArray(fromItem)
-        if (arrTo !== arrFrom) {
-          res[targetKey] = toItem
-        } else if (arrTo && arrFrom) {
-          if (toItem.length < fromItem.length) {
-            res[targetKey] = toItem
-          } else {
-            // 数组
-            diffArrToPath(toItem, fromItem, res, `${targetKey}`)
-          }
+      const arrTo = isArray(toItem);
+      const arrFrom = isArray(fromItem);
+      if (arrTo !== arrFrom) {
+        res[targetKey] = toItem;
+      } else if (arrTo && arrFrom) {
+        if (toItem.length < fromItem.length) {
+          res[targetKey] = toItem;
         } else {
-          if (!toItem || !fromItem || keyList(toItem).length < keyList(fromItem).length) {
-            res[targetKey] = toItem
-          } else {
-            // 对象
-            let shouldDiffObject = true
-            Object.keys(fromItem).some(key => {
-              if (typeof toItem[key] === 'undefined') {
-                shouldDiffObject = false
-                return true
-              }
-            })
-            if (shouldDiffObject) {
-              diffObjToPath(toItem, fromItem, res, `${targetKey}.`)
-            } else {
-              res[targetKey] = toItem
-            }
+          // 数组
+          diffArrToPath(toItem, fromItem, res, `${targetKey}`);
+        }
+      } else if (
+        !toItem ||
+        !fromItem ||
+        keyList(toItem).length < keyList(fromItem).length
+      ) {
+        res[targetKey] = toItem;
+      } else {
+        // 对象
+        let shouldDiffObject = true;
+        Object.keys(fromItem).some(key => {
+          if (typeof toItem[key] === "undefined") {
+            shouldDiffObject = false;
+            return true;
           }
+        });
+        if (shouldDiffObject) {
+          diffObjToPath(toItem, fromItem, res, `${targetKey}.`);
+        } else {
+          res[targetKey] = toItem;
         }
       }
     }
   }
-  return res
+  return res;
 }
 
 // 比较的对象均为plainObject，且函数已被过滤
-export function diffObjToPath (to, from, res = {}, keyPrev = '') {
-  const keys = keyList(to)
-  const len = keys.length
+export function diffObjToPath(to, from, res = {}, keyPrev = "") {
+  const keys = keyList(to);
+  const len = keys.length;
 
   for (let i = 0; i < len; i++) {
-    const key = keys[i]
-    const toItem = to[key]
-    const fromItem = from[key]
-    const targetKey = `${keyPrev}${key}`
+    const key = keys[i];
+    const toItem = to[key];
+    const fromItem = from[key];
+    const targetKey = `${keyPrev}${key}`;
     if (toItem === fromItem) {
-      continue
+      continue;
     } else if (!hasProp.call(from, key)) {
-      res[targetKey] = toItem
+      res[targetKey] = toItem;
     } else if (typeof toItem !== typeof fromItem) {
-      res[targetKey] = toItem
+      res[targetKey] = toItem;
+    } else if (typeof toItem !== "object") {
+      res[targetKey] = toItem;
     } else {
-      if (typeof toItem !== 'object') {
-        res[targetKey] = toItem
-      } else {
-        const arrTo = isArray(toItem)
-        const arrFrom = isArray(fromItem)
-        if (arrTo !== arrFrom) {
-          res[targetKey] = toItem
-        } else if (arrTo && arrFrom) {
-          if (toItem.length < fromItem.length) {
-            res[targetKey] = toItem
-          } else {
-            // 数组
-            diffArrToPath(toItem, fromItem, res, `${targetKey}`)
-          }
+      const arrTo = isArray(toItem);
+      const arrFrom = isArray(fromItem);
+      if (arrTo !== arrFrom) {
+        res[targetKey] = toItem;
+      } else if (arrTo && arrFrom) {
+        if (toItem.length < fromItem.length) {
+          res[targetKey] = toItem;
         } else {
-          // null
-          if (!toItem || !fromItem) {
-            res[targetKey] = toItem
-          } else {
-            // 对象
-            let shouldDiffObject = true
-            Object.keys(fromItem).some(key => {
-              if (typeof toItem[key] === 'undefined') {
-                shouldDiffObject = false
-                return true
-              }
-            })
-            if (shouldDiffObject) {
-              diffObjToPath(toItem, fromItem, res, `${targetKey}.`)
-            } else {
-              res[targetKey] = toItem
+          // 数组
+          diffArrToPath(toItem, fromItem, res, `${targetKey}`);
+        }
+      } else {
+        // null
+        if (!toItem || !fromItem) {
+          res[targetKey] = toItem;
+        } else {
+          // 对象
+          let shouldDiffObject = true;
+          Object.keys(fromItem).some(key => {
+            if (typeof toItem[key] === "undefined") {
+              shouldDiffObject = false;
+              return true;
             }
+          });
+          if (shouldDiffObject) {
+            diffObjToPath(toItem, fromItem, res, `${targetKey}.`);
+          } else {
+            res[targetKey] = toItem;
           }
         }
       }
     }
   }
-  return res
+  return res;
 }
 
-export function queryToJson (str) {
-  const dec = decodeURIComponent
-  const qp = str.split('&')
-  let ret = {}
-  let name
-  let val
+export function queryToJson(str) {
+  const dec = decodeURIComponent;
+  const qp = str.split("&");
+  const ret = {};
+  let name;
+  let val;
   for (let i = 0, l = qp.length, item; i < l; ++i) {
-    item = qp[i]
+    item = qp[i];
     if (item.length) {
-      const s = item.indexOf('=')
+      const s = item.indexOf("=");
       if (s < 0) {
-        name = dec(item)
-        val = ''
+        name = dec(item);
+        val = "";
       } else {
-        name = dec(item.slice(0, s))
-        val = dec(item.slice(s + 1))
+        name = dec(item.slice(0, s));
+        val = dec(item.slice(s + 1));
       }
-      if (typeof ret[name] === 'string') { // inline'd type check
-        ret[name] = [ret[name]]
+      if (typeof ret[name] === "string") {
+        // inline'd type check
+        ret[name] = [ret[name]];
       }
 
       if (isArray(ret[name])) {
-        ret[name].push(val)
+        ret[name].push(val);
       } else {
-        ret[name] = val
+        ret[name] = val;
       }
     }
   }
-  return ret // Object
+  return ret; // Object
 }
 
-const _loadTime = (new Date()).getTime().toString()
-let _i = 1
-export function getUniqueKey () {
-  return _loadTime + (_i++)
+const _loadTime = new Date().getTime().toString();
+let _i = 1;
+export function getUniqueKey() {
+  return _loadTime + _i++;
 }
 
-export function getElementById (component, id, type) {
-  if (!component) return null
+export function getElementById(component, id, type) {
+  if (!component) return null;
 
-  let res
-  if (type === 'component') {
-    res = component.selectComponent(id)
-    res = res ? (res.$component || res) : null
+  let res;
+  if (type === "component") {
+    res = component.selectComponent(id);
+    res = res ? res.$component || res : null;
   } else {
-    const query = wx.createSelectorQuery().in(component)
-    res = query.select(id)
+    const query = wx.createSelectorQuery().in(component);
+    res = query.select(id);
   }
 
-  if (res) return res
+  if (res) return res;
 
-  return null
+  return null;
 }
