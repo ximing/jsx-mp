17a79d43b5db592ab36342996350bb25b8c52749
ximing <ximing@meituan.com>
2019-03-29 13:28:41 +0800

    feat: 补全运行时需要的内部方法

--- packages/jsx-runtime/src/index.js
+++ packages/jsx-runtime/src/index.js
@@ -1,189 +1,173 @@
 /**
  * Created by ximing on 2019-03-23.
  */
-import { isEmptyObject, noop, diffObjToPath } from "./util";
+import { isEmptyObject, noop, diffObjToPath } from './util';
 
-const privatePropValName = "__triggerObserer";
+const privatePropValName = '__triggerObserer';
 const componentFnReg = /^__fn_/;
 
-function filterProps(
-  properties,
-  defaultProps = {},
-  componentProps = {},
-  weappComponentData
-) {
-  const newProps = Object.assign({}, componentProps);
-  for (const propName in properties) {
-    if (propName === privatePropValName) {
-      continue;
+function filterProps(properties, defaultProps = {}, componentProps = {}, weappComponentData) {
+    const newProps = Object.assign({}, componentProps);
+    for (const propName in properties) {
+        if (propName === privatePropValName) {
+            continue;
+        }
+        if (typeof componentProps[propName] === 'function') {
+            newProps[propName] = componentProps[propName];
+        } else if (propName in weappComponentData) {
+            newProps[propName] = weappComponentData[propName];
+        }
+        if (componentFnReg.test(propName)) {
+            if (weappComponentData[propName] === true) {
+                const fnName = propName.replace(componentFnReg, '');
+                newProps[fnName] = noop;
+            }
+            delete newProps[propName];
+        }
     }
-    if (typeof componentProps[propName] === "function") {
-      newProps[propName] = componentProps[propName];
-    } else if (propName in weappComponentData) {
-      newProps[propName] = weappComponentData[propName];
+    if (!isEmptyObject(defaultProps)) {
+        for (const propName in defaultProps) {
+            if (newProps[propName] === undefined || newProps[propName] === null) {
+                newProps[propName] = defaultProps[propName];
+            }
+        }
     }
-    if (componentFnReg.test(propName)) {
-      if (weappComponentData[propName] === true) {
-        const fnName = propName.replace(componentFnReg, "");
-        newProps[fnName] = noop;
-      }
-      delete newProps[propName];
-    }
-  }
-  if (!isEmptyObject(defaultProps)) {
-    for (const propName in defaultProps) {
-      if (newProps[propName] === undefined || newProps[propName] === null) {
-        newProps[propName] = defaultProps[propName];
-      }
-    }
-  }
-  return newProps;
+    return newProps;
 }
 
 function generateObserver(key, ComponentClass, context) {
-  function observer(value) {
-    const nextProps = filterProps(
-      ComponentClass.properties,
-      ComponentClass.defaultProps,
-      Object.assign({}, this.props, {
-        [key]: value
-      }),
-      this.data
-    );
-    if (this.__isReady) {
-      const oldData = Object.assign({}, this.data);
-      // 这里用context不用this的原因是微信会将传递给Component的Object处理一下，其中不在method里面的方法会被移走
-      const newData = context._createData(oldData, nextProps);
-      const dataDiff = diffObjToPath(newData, this.data);
-      this.__nativeSetData(dataDiff);
+    function observer(value) {
+        const nextProps = filterProps(
+            ComponentClass.properties,
+            ComponentClass.defaultProps,
+            Object.assign({}, this.props, {
+                [key]: value
+            }),
+            this.data
+        );
+        if (this.__isReady) {
+            const oldData = Object.assign({}, this.data);
+            const newData = context._createData(oldData, nextProps);
+            const dataDiff = diffObjToPath(newData, this.data);
+            this.__nativeSetData(dataDiff);
+        }
+        this.props = nextProps;
     }
-    this.props = nextProps;
-  }
-  return observer;
+    return observer;
 }
 
 function bindProperties(weappComponentConf, ComponentClass) {
-  weappComponentConf.properties = ComponentClass.properties || {};
-  weappComponentConf.props = weappComponentConf.props || {};
-  Object.keys(weappComponentConf.properties).forEach(key => {
-    weappComponentConf.properties[key] = {
-      ...weappComponentConf.properties[key],
-      value: weappComponentConf.props[key],
-      observer: generateObserver(key, ComponentClass, weappComponentConf)
-    };
-  });
-  const defaultProps = ComponentClass.defaultProps || {};
-  for (const key in defaultProps) {
-    if (defaultProps.hasOwnProperty(key)) {
-      weappComponentConf.properties[key] = {
-        type: null,
-        value: null,
-        observer: generateObserver(key, ComponentClass, weappComponentConf)
-      };
+    weappComponentConf.properties = ComponentClass.properties || {};
+    weappComponentConf.props = weappComponentConf.props || {};
+    Object.keys(weappComponentConf.properties).forEach((key) => {
+        weappComponentConf.properties[key] = {
+            ...weappComponentConf.properties[key],
+            value: weappComponentConf.props[key],
+            observer: generateObserver(key, ComponentClass, weappComponentConf)
+        };
+    });
+    const defaultProps = ComponentClass.defaultProps || {};
+    for (const key in defaultProps) {
+        if (defaultProps.hasOwnProperty(key)) {
+            weappComponentConf.properties[key] = {
+                type: null,
+                value: null,
+                observer: generateObserver(key, ComponentClass, weappComponentConf)
+            };
+        }
     }
-  }
 }
 
 function setData(data, fn = noop) {
-  try {
-    const newData = this._createData(Object.assign({}, this.data, data));
-    const dataDiff = diffObjToPath(newData, this.data);
-    this.__nativeSetData(dataDiff, fn);
-  } catch (err) {
-    console.error("--> setData error", err);
-    throw err;
-  }
+    try {
+        const newData = this._createData(Object.assign({}, this.data, data));
+        const dataDiff = diffObjToPath(newData, this.data);
+        this.__nativeSetData(dataDiff, fn);
+    } catch (err) {
+        console.error('--> setData error', err);
+        throw err;
+    }
 }
 
 export function createComponent(ComponentClass) {
-  const initData = {
-    _componentProps: 1
-  };
-  const componentInstance = new ComponentClass();
-  try {
-    componentInstance.data =
-      componentInstance._createData() || componentInstance.data;
-  } catch (err) {
-    console.warn(
-      `[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`
-    );
-    console.warn(err);
-  }
-  const { created, ready, props, data } = componentInstance;
-  componentInstance.data = Object.assign({}, initData, props, data);
-  componentInstance.__isReady = false;
-  componentInstance.created = function(...args) {
-    this.__nativeSetData = this.setData;
-    Object.defineProperty(this, "setData", {
-      value: setData.bind(this),
-      configurable: true,
-      enumerable: true,
-      writable: false
+    const initData = {
+        _componentProps: 1
+    };
+    const componentInstance = new ComponentClass();
+    try {
+        componentInstance._constructor && componentInstance._constructor();
+        componentInstance.data = componentInstance._createData() || componentInstance.data;
+    } catch (err) {
+        console.warn(`[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`);
+        console.warn(err);
+    }
+    const { created, ready, props, data } = componentInstance;
+    componentInstance.data = Object.assign({}, initData, props, data);
+    componentInstance.__isReady = false;
+    componentInstance.created = function(...args) {
+        this.__nativeSetData = this.setData;
+        Object.defineProperty(this, 'setData', {
+            value: setData.bind(this),
+            configurable: true,
+            enumerable: true,
+            writable: false
+        });
+        this.props = filterProps(
+            ComponentClass.properties,
+            ComponentClass.defaultProps,
+            this.props,
+            this.data
+        );
+        created && created.apply(this, args);
+    };
+    componentInstance.ready = function(...args) {
+        this.__isReady = true;
+        ready && ready.apply(this, args);
+    };
+    // 将原型链上不可枚举的方法赋值过来
+    Object.getOwnPropertyNames(ComponentClass.prototype).forEach((key) => {
+        if (['constructor', 'data', 'props'].indexOf(key) < 0)
+            componentInstance[key] = ComponentClass.prototype[key];
     });
-    this.props = filterProps(
-      ComponentClass.properties,
-      ComponentClass.defaultProps,
-      this.props,
-      this.data
-    );
-    created && created.apply(this, args);
-  };
-  componentInstance.ready = function(...args) {
-    this.__isReady = true;
-    ready && ready.apply(this, args);
-  };
-  // 将原型链上不可枚举的方法赋值过来
-  Object.getOwnPropertyNames(ComponentClass.prototype).forEach(key => {
-    if (
-      ["_constructor", "data", "props", "created", "ready"].indexOf(key) < 0 &&
-      !Object.prototype.hasOwnProperty(key)
-    )
-      componentInstance[key] = ComponentClass.prototype[key];
-  });
-  bindProperties(componentInstance, ComponentClass);
-  return componentInstance;
+    bindProperties(componentInstance, ComponentClass);
+    return componentInstance;
 }
 
-export function createPage(ComponentClass) {
-  const initData = {
-    _componentProps: 1
-  };
-  const componentInstance = new ComponentClass();
-  try {
-    componentInstance.data =
-      componentInstance._createData() || componentInstance.data;
-  } catch (err) {
-    console.warn(
-      `[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`
-    );
-    console.warn(err);
-  }
-  const { onLoad, onReady, props, data } = componentInstance;
-  componentInstance.data = Object.assign({}, initData, props, data);
-  componentInstance.__isReady = false;
-  componentInstance.onLoad = function(...args) {
-    this.__nativeSetData = this.setData;
-    Object.defineProperty(this, "setData", {
-      value: setData.bind(this),
-      configurable: true,
-      enumerable: true,
-      writable: false
+export function createPage(ComponentClass, name) {
+    const initData = {
+        _componentProps: 1
+    };
+    const componentInstance = new ComponentClass();
+    try {
+        componentInstance._constructor && componentInstance._constructor();
+        componentInstance.data = componentInstance._createData() || componentInstance.data;
+    } catch (err) {
+        console.warn(`[JSX warn] 请给组件提供一个 \`defaultProps\` 以提高初次渲染性能！`);
+        console.warn(err);
+    }
+    const { onLoad, onReady, props, data } = componentInstance;
+    if (name) console.log(ComponentClass, 'data', componentInstance);
+    componentInstance.data = Object.assign({}, initData, props, data);
+    componentInstance.__isReady = false;
+    componentInstance.onLoad = function(...args) {
+        this.__nativeSetData = this.setData;
+        Object.defineProperty(this, 'setData', {
+            value: setData.bind(this),
+            configurable: true,
+            enumerable: true,
+            writable: false
+        });
+        onLoad && onLoad.apply(this, args);
+    };
+    componentInstance.onReady = function(...args) {
+        this.__isReady = true;
+        onReady && onReady.apply(this, args);
+    };
+    // 将原型链上不可枚举的方法赋值过来
+    Object.getOwnPropertyNames(ComponentClass.prototype).forEach((key) => {
+        if (['constructor', 'data', 'props'].indexOf(key) < 0)
+            componentInstance[key] = ComponentClass.prototype[key];
     });
-    onLoad && onLoad.apply(this, args);
-  };
-  componentInstance.onReady = function(...args) {
-    this.__isReady = true;
-    onReady && onReady.apply(this, args);
-  };
-  // 将原型链上不可枚举的方法赋值过来
-  Object.getOwnPropertyNames(ComponentClass.prototype).forEach(key => {
-    if (
-      ["_constructor", "data", "props", "onReady", "onReady"].indexOf(key) <
-        0 &&
-      !Object.prototype.hasOwnProperty(key)
-    )
-      componentInstance[key] = ComponentClass.prototype[key];
-  });
 
-  return componentInstance;
+    return componentInstance;
 }
---
+++ packages/jsx-runtime/src/internal/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ximing on 2019-03-28.
+ */
+import { get as internal_safe_get } from './safe-get';
+import { set as internal_safe_set } from './safe-set';
+import { inlineStyle as internal_inline_style } from './inline-style';
+import { getOriginal as internal_get_original } from './get-original';
+
+export { internal_safe_get, internal_safe_set, internal_inline_style, internal_get_original };
