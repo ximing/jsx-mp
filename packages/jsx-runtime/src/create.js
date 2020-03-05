ec8ac7cb3df02789f182a1af696049fd68e8a952
ximing <ximing@meituan.com>
2020-03-05 15:40:46 +0800

    feat: 新增__doUpdate

--- packages/jsx-runtime/src/create.js
+++ packages/jsx-runtime/src/create.js
@@ -123,6 +123,7 @@ export function createComponent(ComponentClass) {
     componentInstance.created = function (...args) {
         this.props = componentInstance.props;
         this.state = componentInstance.state;
+        this.__doUpdate = doUpdate.bind(this);
         componentInstance.__init(this, false);
         created && created.apply(this, args);
     };
@@ -193,6 +194,8 @@ export function createPage(ComponentClass) {
     componentInstance.setState = setState;
     componentInstance.onLoad = function (...args) {
         try {
+            // 暴露给业务做一些更新的事情，比如metric埋点等，需要触发更新
+            this.__doUpdate = doUpdate.bind(this);
             componentInstance.__init(this, true);
         } catch (e) {
             console.error(e);
